2b6202577b14ff8e0a44db78d869ed8dba5843b9
Karo Gebo <k_z.gebo@icloud.com>
2024-10-01 03:47:23 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from blog.models import Post
 
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
     return render(request, "blog/index.html", {"posts": posts})
