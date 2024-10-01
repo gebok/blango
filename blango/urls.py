d545bf078683afbcf8a215672cf23fdae0dc1a55
Karo Gebo <k_z.gebo@icloud.com>
2024-10-01 23:37:18 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.index),
-    path('post/', include('blog.urls')),
+    path('post/<slug>/', blog.views.post_detail, name="blog-post-detail"),
 ]
